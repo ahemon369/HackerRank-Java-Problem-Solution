1aba93648cdc538001de47c878ba56519ba129dd
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2024-11-29 22:34:39 +0600

    Add files via upload

---
+++ Java_Loops_I.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.security.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1; i <=10;i++)
+        {
+            int result= N*i;
+            System.out.printf("%d x %d = %d\n",N,i,result);
+            
+        }
+  }
+}
+
+
+                /*-----------------------------------------------
+                            THIS CODE WAS WRITTEN 
+                                   BY
+                            MD AMRAN HOSSIN EMON
+                
+                -------------------------------------------------*/
